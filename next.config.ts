cb8912224d1604d5ff811e2e72dca0966a976cb7
alinafe c kamwendo <alinafekamwendo11@gmail.com>
2026-03-24 02:05:55 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // Disables type checking during build
+  },
   images: {
     remotePatterns: [
       {
